a855c2046eb6c8f9be238dbc7ff30b44e4cec737
tedvankessel <tvk@us.ibm.com>
2023-05-05 14:25:38 -0400

    added list accounts test and code

--- service/routes.py
+++ service/routes.py
@@ -63,19 +63,30 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+@app.route("/accounts", methods=["GET"])
+def list_accounts():
+    """
+    List all Accounts-tvk
+    This endpoint will list all Accounts
+    """
+    app.logger.info("Request to list Accounts")
+
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+    return jsonify(account_list), status.HTTP_200_OK
 
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
 
 # ... place you code here to READ an account ...
-    ######################################################################
-    # READ AN ACCOUNT
-    ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
-    Reads an Account
+    Reads an Account-tvk
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
@@ -93,7 +104,7 @@ def get_accounts(account_id):
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
-    Update an Account
+    Update an Account-tvk
     This endpoint will update an Account based on the posted data
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
