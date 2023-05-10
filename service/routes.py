e9aa8d1efa36e6344e4a0381f1a5419bc9098634
tedvankessel <tvk@us.ibm.com>
2023-05-10 17:01:29 -0400

    corrected flake8 errors

--- service/routes.py
+++ service/routes.py
@@ -63,6 +63,7 @@ def create_accounts():
 
 # ... place you code here to LIST accounts ...
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -83,6 +84,7 @@ def list_accounts():
 
 # ... place you code here to READ an account ...
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -100,6 +102,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -123,6 +126,7 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
